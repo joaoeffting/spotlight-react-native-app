e7c73c0f8f3696299ce62d71380b6e2ed0109ed8
joaeff <joaopaulo.effting@kindredgroup.com>
2025-10-22 21:05:02 +0200

    Changing splash screen and icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,29 @@
 import InitialLayout from "@/components/InitialLayout";
 import { COLORS } from "@/constants/theme";
-
 import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
+import { useFonts } from "expo-font";
+import { SplashScreen } from "expo-router";
+import { useEffect } from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("@/assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    // Render nothing (keep splash screen visible)
+    return null;
+  }
+
   return (
     <ClerkAndConvexProvider>
       <SafeAreaProvider>
